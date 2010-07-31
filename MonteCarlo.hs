d9ec2e20aa8c297d0ca7b3e2b8710be1b7d5e5a4
phil <phil@beadling.co.uk>
2010-07-31 19:45:55 +0000

    Basic Gnuplot graphing

--- MonteCarlo.hs
+++ MonteCarlo.hs
@@ -1,9 +1,21 @@
+-- Gnuplot imports
+import qualified Graphics.Gnuplot.Advanced as Plot
+import qualified Graphics.Gnuplot.Terminal.X11 as X11
+import qualified Graphics.Gnuplot.Frame as Frame
+import qualified Graphics.Gnuplot.Frame.Option as Opt
+import qualified Graphics.Gnuplot.Frame.OptionSet as Opts
+import qualified Graphics.Gnuplot.Graph as Graph
+import qualified Graphics.Gnuplot.Plot.ThreeDimensional as Plot3D
+import qualified Graphics.Gnuplot.Graph.ThreeDimensional as Graph3D
+import Graphics.Gnuplot.Plot.TwoDimensional (linearScale, )
 
+-- Local imports
 import Parallel
 import Data
 import Instruments
 import Rngs
 
+-- Monte Carlo Stuff
 mc :: MonteCarloUserData -> [[[Double]]] -> Double
 mc userData rndsss = 
   (parSum $ foldChunks f 0 rndsss) / (fromIntegral $ numOfSims userData)
@@ -14,18 +26,51 @@ mc userData rndsss =
 discount userData = (*) (exp ( (-(interestRate userData)) * 
                              (expiry userData) ))  
 
-main :: IO()
-main = do
-  let userData = MonteCarloUserData { stock        = ExistentialInstrument $ Asian (100,100),
+-- Option Wrappers
+european expiry stock =
+  let userData = MonteCarloUserData { stock        = ExistentialInstrument $ European stock,
                                       strike       = 100,
                                       putCall      = Call,
                                       volatility   = 0.2,
-                                      expiry       = 1,
+                                      expiry       = expiry,
                                       interestRate = 0.05,
-                                      timeSteps    = 500,
-                                      numOfSims    = 100000 }
-  
-  let rngsss = randomChunks ranq1Norm userData 20 -- discard first 20
-    in print $ discount userData $ mc userData rngsss
+                                      timeSteps    = 1,
+                                      numOfSims    = 10000 }
+                 in discount userData $ mc userData (randomChunks haltonNorm userData 20)
+                    
+asian expiry stock =
+  let userData = MonteCarloUserData { stock        = ExistentialInstrument $ Asian (stock,stock),
+                                      strike       = 100,
+                                      putCall      = Call,
+                                      volatility   = 0.2,
+                                      expiry       = expiry,
+                                      interestRate = 0.05,
+                                      timeSteps    = 100,
+                                      numOfSims    = 1000 }
+                 in discount userData $ mc userData (randomChunks ranq1Norm userData 20)
 
 
+-- Graph functions
+defltOpts :: Graph.C graph => Opts.T graph
+defltOpts =
+   Opts.remove Opt.key $
+   Opts.deflt
+
+euro3d :: Frame.T (Graph3D.T Double Double Double)
+euro3d =
+   let meshStock  = linearScale 35 (50,150)
+       meshExpiry = linearScale 35 (0,5)
+   in  Frame.cons
+          (Opts.xRange3d (0,5) $
+           Opts.yRange3d (50,150) $
+           defltOpts)  $
+       Plot3D.surface
+          meshExpiry meshStock european
+          
+
+-- HINT: ghc -O2 -threaded --make MonteCarlo.hs
+--            ./MonteCarlo +RTS -N2 
+main :: IO()
+main = do 
+  Plot.plot X11.cons euro3d
+  print "end"
